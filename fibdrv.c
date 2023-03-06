733448086240f146f9cb235c42454c1b691f8f00
linhoward0522 <linhoward71@gmail.com>
2023-03-06 22:01:50 +0800

    Implement avoid VLA and fix pre-commit.hook
    VLA is not allowed in Linux kernel. Fix pre-commit hook.

--- fibdrv.c
+++ fibdrv.c
@@ -27,16 +27,17 @@ static DEFINE_MUTEX(fib_mutex);
 static long long fib_sequence(long long k)
 {
     /* FIXME: C99 variable-length array (VLA) is not allowed in Linux kernel. */
-    long long f[k + 2];
+    if (k < 2)
+        return k;
 
-    f[0] = 0;
-    f[1] = 1;
+    long long a = 0, b = 1;
 
     for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
+        long long c = a + b;
+        a = b;
+        b = c;
     }
-
-    return f[k];
+    return b;
 }
 
 static int fib_open(struct inode *inode, struct file *file)
