3e2fe44b3d04e244e0f69a267e803e7ab76ffca5
linhoward0522 <linhoward71@gmail.com>
2023-03-08 13:13:22 +0800

    Implement kernel mode time measurement
    Implement in fib_write.

--- fibdrv.c
+++ fibdrv.c
@@ -4,6 +4,7 @@
 #include <linux/init.h>
 #include <linux/kdev_t.h>
 #include <linux/kernel.h>
+#include <linux/ktime.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 
@@ -137,7 +138,11 @@ static ssize_t fib_write(struct file *file,
                          size_t size,
                          loff_t *offset)
 {
-    return 1;
+    ktime_t kt;
+    kt = ktime_get();
+    fib_sequence(*offset);
+    kt = ktime_sub(ktime_get(), kt);
+    return (ssize_t) ktime_to_ns(kt);
 }
 
 static loff_t fib_device_lseek(struct file *file, loff_t offset, int orig)
